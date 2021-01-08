6e89eaa9e4bd61a27c8af09ffd478d8d9959d404
Alexis BORONAT <alexis.boronat@desmazieres.fr>
2021-01-08 14:53:32 +0100

    ajout dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -8,6 +10,14 @@ import { MainContentInnerComponent } from './main-content-inner/main-content-inn
 import { ChatSidebarComponent } from './chat-sidebar/chat-sidebar.component';
 import { RessourceComponent } from './ressource/ressource.component';
 import { PostComponent } from './post/post.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+// Import matérial
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -17,10 +27,19 @@ import { PostComponent } from './post/post.component';
     MainContentInnerComponent,
     ChatSidebarComponent,
     RessourceComponent,
-    PostComponent
+    PostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    // Matérial
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   constructor() { }
+  public menu = [
+    { icone: 'home', nom: 'Fil d\'actualité', lien: '/actu' },
+    { icone: 'article', nom: 'Articles populaires', lien: '/actu' },
+    { icone: 'event', nom: 'Événements', lien: '/events' },
+    { icone: 'gamepad', nom: 'Jeux', lien: '/jeu' },
+    { icone: 'person_search', nom: 'Retrouver des amis', lien: '/friends' },
+    { icone: 'explore', nom: 'Explorer', lien: '/groupe' }
+  ];
 
   ngOnInit(): void {
   }
