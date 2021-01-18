ce35b70c108b1314b9454918ac5b6b9aee9c07f3
Alexis BORONAT <alexis.boronat@desmazieres.fr>
2021-01-18 11:57:54 +0100

    uniformisation default theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { RessourceComponent } from './ressource/ressource.component';
 import { GameComponent } from './game/game.component';
@@ -24,26 +23,24 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent,
     MainContentComponent,
     RessourceComponent,
     GameComponent,
     GroupComponent,
     RegisterComponent,
     ChatComponent,
-    ToolbarComponent
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserModule,
     AppRoutingModule,
-    BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     // Matérial
@@ -55,6 +52,7 @@ import { MatInputModule } from '@angular/material/input';
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  @Output() openChat = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit(): void {
